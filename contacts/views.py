4e1761d7473d321e5b8f499dcd8a36b463d0a09b
sadeqEbrahim <164956595+sadeqEbrahim@users.noreply.github.com>
2025-08-19 04:14:47 +0200

    Update views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from .models import Contact
 from .serializers import ContactSerializer
 
-class ContactViewSet(viewsets.ModelViewSet):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerializer
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def contact_list_create(request):
+    if request.method == 'GET':
+        contacts = Contact.objects.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
