ff05cee3e0a4253223eff9edd0dd7805e8c79460
sadeqEbrahim <164956595+sadeqEbrahim@users.noreply.github.com>
2025-08-19 04:14:04 +0200

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    message = models.TextField()
 
     def __str__(self):
         return self.name
