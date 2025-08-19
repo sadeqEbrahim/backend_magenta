36c7518ba61e809e9f152cd3d6fce7a832249090
sadeqEbrahim <164956595+sadeqEbrahim@users.noreply.github.com>
2025-08-19 04:15:23 +0200

    Update urls.py

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,20 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ContactViewSet
-
-router = DefaultRouter()
-router.register(r'contacts', ContactViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
-
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from contacts.views import contact_list_create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('contacts.urls')),  # API endpoint
+    path('api/contacts/', contact_list_create),
 ]
